a9b42b81a92678cc4d41736afb2d711903e8d6c3
MarcosGuilhermeAraujo <marcosedaviaraujo@gmail.com>
2024-03-18 08:47:24 -0300

    Editando nome da variavel nome para nome1

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,5 @@
 public class Cliente {
-    private String tipoConta;
-    private String nome;
+    private String nome1;
     private String cpf;
     private int agencia;
     private double saldo;
